c85bc6bdee9af5f5563c8dffac23c25e37eef6dd
tracy8 <tracymurenzi@gmail.com>
2024-11-19 20:46:21 +0200

    fix-footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
   
       <footer className="bg-[#F6F7FC] absolute right-0 left-0 bottom-0 w-full">
   <div className="container mx-auto flex mt-10">
-    <img src={heriworkLogo} alt="heriwork logo" className="w-48 h-48" />
+    <img src={heriworkLogo} alt="heriwork logo" className="h-48 pr-20" />
     <div className="w-1/3 space-y-2">
-      <h4 className="text-lg font-bold">Quick Links</h4>
-      <ul className="space-y-1]">
+      <h4 className="text-lg font-bold text-start">Quick Links</h4>
+      <ul className="space-y-1 text-start">
         <li><a href="#" className="hover:text-gray-300 text-[#7A7A7A]">Home</a></li>
         <li><a href="#" className="hover:text-gray-300 text-[#7A7A7A]">Services</a></li>
         <li><a href="#" className="hover:text-gray-300 text-[#7A7A7A]">About Us</a></li>
@@ -19,7 +19,7 @@ const Footer = () => {
       </ul>
     </div>
     <div className="w-1/3 space-y-2">
-      <h4 className="text-lg font-bold">Get In touch</h4>
+      <h4 className="text-lg font-bold text-start">Get In touch</h4>
       <ul className="space-y-1">
         <li><a href="#" className="flex items-center hover:text-gray-300 text-[#7A7A7A]"><i className="fab fa-instagram mr-2"></i>heriwork250</a></li>
         <li><a href="#" className="flex items-center hover:text-gray-300 text-[#7A7A7A]"><i className="fab fa-twitter mr-2"></i>heriwork250</a></li>
@@ -28,7 +28,7 @@ const Footer = () => {
       </ul>
     </div>
     <div className="w-1/3">
-      <h4 className="font-bold">More Info</h4>
+      <h4 className="font-bold text-start">More Info</h4>
       <p className='text-[#7A7A7A] text-left'>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
     </div>
   </div>
