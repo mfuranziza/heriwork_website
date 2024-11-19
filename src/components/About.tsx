6404c73a616e5debb7cdb71d629f60e66a35c84e
Aimelyse Moss <amosekata@gmail.com>
2024-11-20 00:55:26 +0200

    Feat: Add Description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,8 +13,18 @@ const About = () => {
       />
     </div>
 
-    
+    <div className="w-full md:w-1/2 text-center md:text-left">
+      <h2 className="text-3xl font-semibold text-gray-800">
+        About <span className="text-[#DD6A18]">Us</span>
+      </h2>
+      <p className="mt-4 text-[#7A7A7A] leading-relaxed">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+      </p>
+      <button className="mt-6 bg-[#04192D] text-white px-6 py-2 rounded-lg">
 
+        Contact Us
+      </button>
+    </div>
   </div>
 </div>
 
