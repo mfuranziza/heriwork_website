44f38f6542180e02a536b7a95d43bffc923eff06
Aimelyse Moss <amosekata@gmail.com>
2024-11-27 19:55:45 +0200

    Fix: Whole Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,40 @@
-
-import { Home } from './components/Home'
-import './App.css'
-import Footer from './components/Footer'
-import ContactForm from './components/Contact-us'
-import About from './components/About'
-import Testimonials from './components/testimonials'
-import Partners from './components/Partners'
-import Team from './components/Team'
+import { Home } from "./components/Home";
+import "./App.css";
+import Footer from "./components/Footer";
+import ContactForm from "./components/Contact-us";
+import About from "./components/About";
+import Testimonials from "./components/testimonials";
+import Partners from "./components/Partners";
+import Team from "./components/Team";
 
 function App() {
-
   return (
     <>
-   <div className='  font-poppins '>
-    <Home/>
-   </div>
-   <div className="font-poppins bg-gray-50">
-      <div className="w-full">
-        <About />
+      <div className="  font-poppins ">
+        <Home />
       </div>
-      <div className="w-full mt-12">
-        <Partners />
+      <div className="font-poppins bg-white">
+        <div className="w-full">
+          <About />
+        </div>
+        <div className="w-full">
+          <Partners />
+        </div>
+        <div className="">
+          <Team />
+        </div>
+        <div className="">
+          <Testimonials />
+        </div>
+        <div className="">
+          <ContactForm />
+        </div>
+        <div className="">
+          <Footer />
+        </div>
       </div>
-      <div className="mt-12">
-        <Testimonials />
-      </div>
-      <div className="mt-12">
-        <ContactForm />
-      </div>
-      <div className="mt-12">
-        <Footer />
-      </div>
-      <div className="w-full mt-12">
-        <Team />
-      </div>
-    </div>
-      
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
